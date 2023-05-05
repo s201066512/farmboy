8f25b7d72dfbe7c2d266f430219d02acf4ecd81e
AidenTenn <s201066512@ddsbstudent.ca>
2023-05-05 10:03:00 -0400

    added loop

--- ScheduleSender.py
+++ ScheduleSender.py
@@ -26,60 +26,60 @@ from datetime import timedelta, date
 import smtplib
 from email.message import EmailMessage
 import re
+while (True):
+    # email setup
+    msg = EmailMessage()
+    msg['Subject'] = 'Shift Reminder'
+    msg['From'] = "farmboyShiftReminder@gmail.com"
+    msg['To'] = "aiden.ten30@gmail.com"
 
-# email setup
-msg = EmailMessage()
-msg['Subject'] = 'Shift Reminder'
-msg['From'] = "farmboyShiftReminder@gmail.com"
-msg['To'] = "aiden.ten30@gmail.com"
+    tomorrow = date.today() + timedelta(days=1) # get current day
 
-tomorrow = date.today() + timedelta(days=1) # get current day
+    # Enter email
+    driver = webdriver.Firefox()
+    driver.get("https://myfarmboy.ca/login/")
+    email = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/div/div[1]/input")
+    email.clear()
+    email.send_keys("aiden.ten30@gmail.com")
 
-# Enter email
-driver = webdriver.Firefox()
-driver.get("https://myfarmboy.ca/login/")
-email = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/div/div[1]/input")
-email.clear()
-email.send_keys("aiden.ten30@gmail.com")
+    # Enter password
+    password = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/div/div[2]/input")
+    password.clear()
+    password.send_keys("N@th@n1o")
+    password.send_keys(Keys.RETURN)
 
-# Enter password
-password = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/div/div[2]/input")
-password.clear()
-password.send_keys("N@th@n1o")
-password.send_keys(Keys.RETURN)
+    driver.implicitly_wait(10) # wait up to 10 seconds for page to load 
 
-driver.implicitly_wait(10) # wait up to 10 seconds for page to load 
-
-latestShift = driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div/div[1]/div/div/p")
-latestShiftDay = re.findall(r'\d+', latestShift.text) # get all the numerical values 
-if latestShiftDay[0] == tomorrow: # check if I have a shift tomorrow
+    latestShift = driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/div/div[1]/div/div/p")
+    latestShiftDay = re.findall(r'\d+', latestShift.text) # get all the numerical values 
+    if latestShiftDay[0] == tomorrow: # check if I have a shift tomorrow
     # send email
-    msg.set_content(latestShift.text)
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login("farmboyShiftReminder@gmail.com", "uittsrygrdlhouny")
-    server.send_message(msg)
-    server.quit()
-else:
-    print("No Shift Tomorrow")
+        msg.set_content(latestShift.text)
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login("farmboyShiftReminder@gmail.com", "uittsrygrdlhouny")
+        server.send_message(msg)
+        server.quit()
+    else:
+        print("No Shift Tomorrow")
 
-driver.find_element(By.XPATH, "/html/body/div/div/div/nav/div/div/div[1]/div[3]/div/a[2]").click() # click on schedule
-time.sleep(3) # wait a bit
-old_shifts = re.findall(r'(\d{1,2}:\d{2} [AP]M) to (\d{1,2}:\d{2} [AP]M)', driver.page_source) # get the times
-time.sleep(5) # wait (maybe an hour?)
-shifts = re.findall(r'(\d{1,2}:\d{2} [AP]M) to (\d{1,2}:\d{2} [AP]M)', driver.page_source) # get them again
-try:
-    for x in range(len(shifts)): # loop through them
-        if shifts[x] != old_shifts[x]: # see if anything was changed during that time
-            print("Shift at " + str(old_shifts[x]) + " has been changed to " + str(shifts[x]))
+    driver.find_element(By.XPATH, "/html/body/div/div/div/nav/div/div/div[1]/div[3]/div/a[2]").click() # click on schedule
+    time.sleep(10) # wait a bit
+    old_shifts = re.findall(r'(\d{1,2}:\d{2} [AP]M) to (\d{1,2}:\d{2} [AP]M)', driver.page_source) # get the times
+    time.sleep(3600) # wait (maybe an hour?)
+    shifts = re.findall(r'(\d{1,2}:\d{2} [AP]M) to (\d{1,2}:\d{2} [AP]M)', driver.page_source) # get them again
+    try:
+        for x in range(len(shifts)): # loop through them
+            if shifts[x] != old_shifts[x]: # see if anything was changed during that time
+                print("Shift at " + str(old_shifts[x]) + " has been changed to " + str(shifts[x]))
 
-            # if there were changes send an email with what was changed
-            msg.set_content("Shift at " + str(old_shifts[x]) + " has been changed to " + str(shifts[x]))
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.login("farmboyShiftReminder@gmail.com", "uittsrygrdlhouny")
-            server.send_message(msg)
-            server.quit()
-except IndexError:
-    print("IndexError (ignore)")
+                # if there were changes send an email with what was changed
+                msg.set_content("Shift at " + str(old_shifts[x]) + " has been changed to " + str(shifts[x]))
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.login("farmboyShiftReminder@gmail.com", "uittsrygrdlhouny")
+                server.send_message(msg)
+                server.quit()
+    except IndexError:  
+        print("IndexError (ignore)")
 
     """
     This will give me index out of bounds error if the old_shifts includes a week and shifts doesn't include that week
@@ -88,4 +88,5 @@ except IndexError:
     So there might just be a slight delay
     Still need to run it constantly and figure out how I want to do that
     """
-driver.close()
+    driver.close()
+    time.sleep(43200)
